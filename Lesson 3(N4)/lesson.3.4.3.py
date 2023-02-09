571f1b63c8096b417201344a31925acee4c72b13
mihaelmojseiev <mihael.mojseiev@ab-soft.net>
2023-02-09 10:59:08 +0100

    10*58*9*2*23

--- Lesson 3(N4)/lesson.3.4.3.py
+++ Lesson 3(N4)/lesson.3.4.3.py
@@ -1,8 +1,20 @@
 # 3 There is a string " name=Amanda=sssss&age=32&&salary=1500&currency=euro ".
 # Convert this string to a dictionary {name: Amanda, age: 32, salary: 1500, currency: quro}
-import json
+import ast
 tias = " name=Amanda=sssss&age=32&&salary=1500&currency=euro "
-pok = tias.replace(" ","").split("&")
 
-print(pok)
-
+pok = tias.replace(' ','').split('&')
+while '' in pok:
+  pok.remove('')
+pok = [x.split('=sssss')[0] for x in pok]
+pok = str(pok).split('=')
+stringg = ''
+for x in pok:
+    stringg += ' '+ x
+print(stringg)
+# keys = stringg.split(", ")
+# dictionary = {}
+# for i in range(len(keys)):
+    # dictionary[keys[i]]
+# print(dictionary)
+# пока хз как доделать
