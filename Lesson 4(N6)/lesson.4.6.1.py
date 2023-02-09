f1c64742c37b6b7d97788d57366cd3acd5304ea4
mihaelmojseiev <mihael.mojseiev@ab-soft.net>
2023-02-09 13:38:37 +0100

    1*35*9*2*23

--- Lesson 4(N6)/lesson.4.6.1.py
+++ Lesson 4(N6)/lesson.4.6.1.py
@@ -1,4 +1,16 @@
 # 1 You have a list of elements [1, 2, 3, 4, 5, 6, 7, 8].
 # Loop through the list using the foreach loop.
-# The element with an odd index is put into a new list of tuples where the first element is the index and the second is the value.
-# [(index, value)]. accordingly, elements with an even index are placed in another list of tuples with the same format as in the case with odd indices.
+# The element with an odd index is put into a new list of tuples where the first element is the index and the second is the value. [(index, value)].
+# accordingly, elements with an even index are placed in another list of tuples with the same format as in the case with odd indices.
+
+n = [1, 2, 3, 4, 5, 6, 7, 8]
+l = len(n)
+even = []
+odd = []
+for i in range(l):
+    if i % 2 == 0:
+        odd.append(n[i])
+    else:
+        even.append(n[i])
+for index in range(len(n)):
+    print('Индекс: ', index, 'Число: ', n[index])
--- Lesson 4(N6)/lesson.4.6.2.py
+++ Lesson 4(N6)/lesson.4.6.2.py
@@ -3,3 +3,13 @@
 # Loop through the names of friends and write the message f"{friend} we are the best friends"
 # if the friend is not in the list of enemy names. And display the message f"{friend} we are not friends anymore"
 # if the friend is on the list of enemies. If the friend's name is James, we don't check him because he is the best friend.
+
+friends = ["John", "Marta", "James"]
+enemies = ["John", "Johnatan", "Artur"]
+
+for name in friends:
+    print(f"{name} we are the best friends")
+    if name in enemies:
+        print(f"{name} we don't check him because he is the best friend") #тут хз как лучше, ф функцию по нейму или всё таки по индексу
+    if name not in enemies:
+        print(f"{name} we are not friends anymore")
