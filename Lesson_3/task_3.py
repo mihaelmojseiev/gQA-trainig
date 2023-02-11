6b4644c5891aa82553ef30173b2e9fc66033b525
mihaelmojseiev <mihael.mojseiev@ab-soft.net>
2023-02-12 00:43:56 +0100

    upd

--- Lesson_3/task_3.py
+++ Lesson_3/task_3.py
@@ -1,26 +1,9 @@
 # 3 There is a string " name=Amanda=sssss&age=32&&salary=1500&currency=euro ".
 # Convert this string to a dictionary {name: Amanda, age: 32, salary: 1500, currency: quro}
-import ast
-import json
 tias = " name=Amanda=sssss&age=32&&salary=1500&currency=euro "
-# tias = tias.replace('&','').split('=sssss')
-tias = tias.replace(' ','').replace('&',' ').replace('=sssss',' ').replace('  ',' ').replace(' ',';')
-dictionary = dict(subString.split("=") for subString in tias.split(";"))
-print(dictionary)
-print(tias)
 
-#
-# tias = tias.replace(' ','').split('&')
-#
-# while '' in tias:
-#   tias.remove('')
-#
-# tias = [x.split('=sssss')[0] for x in tias]
-# tias = strс(tias).split('=')
-# stringg = ''
-#
-# for x in tias:
-#     stringg += ' '+ x
-#
-# strr = stringg.replace(',', '').replace('[','').replace(']','').replace("'", '')
-# print(strr)
+tias = tias.replace(' ','').replace('&',' ').replace('=sssss',' ').replace('  ',' ').replace(' ',';')
+tias = dict(subString.split("=")
+                  for subString in tias.split(";"))
+
+print(tias)
