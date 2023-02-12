c9025ab61d9ce674806f11bff46d0655b9c54e49
mihaelmojseiev <mihael.mojseiev@ab-soft.net>
2023-02-12 01:06:47 +0100

    upd

--- Lesson_5/task_1.py
+++ Lesson_5/task_1.py
@@ -1,2 +1,27 @@
 # 1. Write a code with all types of loops:
 # Array with any data and loop that can handle it.
+
+loop1 = ['apple', 'macbook', 'pro']
+
+for i in loop1:
+    print(i)
+
+
+loop2 = 1
+
+while loop2 < 3:
+    print(loop2)
+    loop2 = loop2+1
+
+
+loop3 = ["apple", "macbook"]
+loop3num = [1, 2]
+
+for i in loop3:
+    for o in loop3num:
+        print(i,o)
+
+if loop1 == loop3:
+    print(True)
+else:
+    print(False)
--- Lesson_5/task_2.py
+++ Lesson_5/task_2.py
@@ -1,2 +1,6 @@
 # 2. You have a variable var=10. Write other variable 'result' which
 # will get string True if var == 10 and False in other case. Using Tern operators
+
+var=10
+result = True if var == 10 else False
+print(result)
